45acc81e7b683f1a613a4abe69e16c1b4fea3dad
tobitokuye-creator <tobitokuye@gmail.com>
2025-10-30 16:27:42 +0100

    Update nginx-entrypoint.sh

--- nginx-entrypoint.sh
+++ nginx-entrypoint.sh
@@ -1,30 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
-
-# This script runs inside the nginx container at startup (mounted into /docker-entrypoint.d)
-# It renders the nginx.conf from template using ACTIVE_POOL to choose primary/backup mapping.
-
-
 ACTIVE_POOL=${ACTIVE_POOL:-blue}
 BLUE_HOST=${BLUE_HOST:-app_blue:3000}
 GREEN_HOST=${GREEN_HOST:-app_green:3000}
 
-
 if [ "$ACTIVE_POOL" = "blue" ]; then
-PRIMARY_HOST="$BLUE_HOST"
-SECONDARY_HOST="$GREEN_HOST"
+  PRIMARY_HOST="$BLUE_HOST"
+  SECONDARY_HOST="$GREEN_HOST"
 else
-PRIMARY_HOST="$GREEN_HOST"
-SECONDARY_HOST="$BLUE_HOST"
+  PRIMARY_HOST="$GREEN_HOST"
+  SECONDARY_HOST="$BLUE_HOST"
 fi
 
+mkdir -p /etc/nginx/conf.d /var/cache/nginx /var/run
 
-# Populate template
-envsubst '$PRIMARY_HOST $SECONDARY_HOST' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+# Render template
+envsubst '\$PRIMARY_HOST \$SECONDARY_HOST' \
+  < /etc/nginx/templates/nginx.conf.template \
+  > /etc/nginx/conf.d/default.conf
 
-# Ensure permissions
-chown -R nginx:nginx /var/cache/nginx /var/run
-
-
-# Let the official entrypoint continue (nginx -g 'daemon off;')
+nginx -g 'daemon off;'
